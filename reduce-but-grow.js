293da472da3b58542ad2b89b7b9909ca6de5f106
iontherata <93342943+iontherata@users.noreply.github.com>
2022-08-09 20:43:22 +0300

    Add files via upload
    All codewars katas.

---
+++ reduce-but-grow.js
@@ -0,0 +1,14 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// My solution:
+
+
+function grow(x) {
+    total = 1;
+    for (let units of x) {
+        total *= units
+    }
+    return total
+}
