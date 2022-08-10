ce0e5eb205d988cfbb6cb97432484995751396bc
iontherata <93342943+iontherata@users.noreply.github.com>
2022-08-10 18:42:58 +0300

    Update a_needle_in_the_haystack.js

--- a_needle_in_the_haystack.js
+++ a_needle_in_the_haystack.js
@@ -17,10 +17,17 @@
 function findNeedle(haystack) {
 
     for (let i = 0; i < haystack.length; i++) {
-        if (haystack === 'needle') {
-            return haystack;
+        if (haystack[i] === "needle") {
+            return `found the needle at position ${haystack.indexOf("needle") + 1}`
         }
 
     }
+}
 
+
+
+// THE SHORT VERSION:
+
+function findNeedle(haystack) {
+    return "found the needle at position " + haystack.indexOf("needle");
 }
