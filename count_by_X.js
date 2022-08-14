61564de4e24d4f45c366b7d5d3db19879deec22a
iontherata <93342943+iontherata@users.noreply.github.com>
2022-08-14 11:46:42 +0300

    Add files via upload

---
+++ count_by_X.js
@@ -0,0 +1,26 @@
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+// My solution:
+
+
+function countBy(x, n) { //3, 4   === 3 6 9 12 
+    let z = [];
+    let total = x * n
+
+    for (let i = x; i <= total; i += x) {
+        z.push(i)
+    }
+    return z
+}
+
+
+
